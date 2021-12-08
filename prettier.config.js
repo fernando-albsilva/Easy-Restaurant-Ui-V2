9d473f2770de204b249ddc24e06ab0a71516b7cb
fernando <fernando.albsilva@gmail.com>
2021-12-08 13:21:14 -0300

    ajustando config do prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,16 +1,8 @@
 module.exports = {
-  printWidth: 100,
-  tabWidth: 2,
-  useTabs: false,
-  semi: false,
-  singleQuote: true,
+  semi: true,
   trailingComma: 'all',
-  bracketSpacing: true,
-  jsxBracketSameLine: false,
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
   arrowParens: 'avoid',
-  rangeStart: 0,
-  rangeEnd: Infinity,
-  requirePragma: false,
-  insertPragma: false,
-  proseWrap: 'preserve',
-}
+};
