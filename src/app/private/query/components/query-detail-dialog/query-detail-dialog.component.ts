6f3f243cb042e31a7e0be97858601ff6318bcc5a
fernando <fernando.albsilva@gmail.com>
2022-02-28 11:58:41 -0300

    ajustando formatacao

--- src/app/private/query/components/query-detail-dialog/query-detail-dialog.component.ts
+++ src/app/private/query/components/query-detail-dialog/query-detail-dialog.component.ts
@@ -3,87 +3,58 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { InvoiceItemModel, InvoiceModel } from '../../model/invoice.model';
 
 @Component({
-  selector: 'app-query-detail-dialog',
-  templateUrl: './query-detail-dialog.component.html',
-  styleUrls: ['./query-detail-dialog.component.scss']
+    selector: 'app-query-detail-dialog',
+    templateUrl: './query-detail-dialog.component.html',
+    styleUrls: ['./query-detail-dialog.component.scss'],
 })
 export class QueryDetailDialogComponent implements OnInit {
+    public totalValue: number = 0;
+    public invoiceItems: Array<InvoiceItemModel> = [];
+    public displayedColumns: string[] = ['productName', 'quantity', 'unitValue', 'cost', 'total'];
 
-  public totalValue: number = 0;
-  public invoiceItems: Array<InvoiceItemModel> = [];
-  public displayedColumns: string[] = [
-    'productName',
-    'quantity',
-    'unitValue',
-    'cost',
-    'total',
-  ];
+    constructor(
+        public dialogRef: MatDialogRef<QueryDetailDialogComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: InvoiceModel,
+    ) {}
 
-  constructor(
-    public dialogRef: MatDialogRef<QueryDetailDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: InvoiceModel,
-  ) { }
-
-  ngOnInit(): void {
-    console.log("dialogo")
-    console.log(this.data)
-    this.calcTotalValue()
-    if (this.data.invoiceItems) {
-      this.invoiceItems = this.data.invoiceItems;
-    }
-  }
-
-  public getTenPerCentOfTotalValue = (): string => {
-    return (
-      (this.totalValue * 0.1)
-      .toFixed(2)
-    );
-  }
-  
-  public getTotalValueWithTenPerCent = (): string => {
-    return (
-      (this.totalValue * 1.1)
-      .toFixed(2)
-      .toString()
-    );
-  }
-  
-  public getInvoiceBalance = (): string => {
-    return (
-      (
-        (this.totalValue * 1.1) - Number(this.getInvoiceExpense())
-      )
-      .toFixed(2)
-      .toString()
-    );
-  }
-
-  public getInvoiceExpense = (): string =>{
-      let totalExpense = 0;
-
-      this.data.invoiceItems.forEach(
-        (product: InvoiceItemModel)=>{
-          totalExpense += (product.quantity * product.cost);
+    ngOnInit(): void {
+        console.log('dialogo');
+        console.log(this.data);
+        this.calcTotalValue();
+        if (this.data.invoiceItems) {
+            this.invoiceItems = this.data.invoiceItems;
         }
-      );
-      
-      return(
-       totalExpense
-        .toFixed(2)
-        .toString()
-      );
-  }
+    }
 
-  private calcTotalValue = (): void => {
-    let total = 0;
+    public getTenPerCentOfTotalValue = (): string => {
+        return (this.totalValue * 0.1).toFixed(2);
+    };
 
-    this.data.invoiceItems.forEach(
-      (product: InvoiceItemModel) =>{
-         total += product.unitValue * product.quantity;
-      }
-    );
-    
-    this.totalValue = total;
-  }
+    public getTotalValueWithTenPerCent = (): string => {
+        return (this.totalValue * 1.1).toFixed(2).toString();
+    };
 
+    public getInvoiceBalance = (): string => {
+        return (this.totalValue * 1.1 - Number(this.getInvoiceExpense())).toFixed(2).toString();
+    };
+
+    public getInvoiceExpense = (): string => {
+        let totalExpense = 0;
+
+        this.data.invoiceItems.forEach((product: InvoiceItemModel) => {
+            totalExpense += product.quantity * product.cost;
+        });
+
+        return totalExpense.toFixed(2).toString();
+    };
+
+    private calcTotalValue = (): void => {
+        let total = 0;
+
+        this.data.invoiceItems.forEach((product: InvoiceItemModel) => {
+            total += product.unitValue * product.quantity;
+        });
+
+        this.totalValue = total;
+    };
 }
